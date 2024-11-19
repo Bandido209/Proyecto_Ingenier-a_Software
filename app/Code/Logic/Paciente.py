8850b3865de569ae93ea66346488f4a8ea1d00dd
Bandido209 <estebananm2005@gmail.com>
2024-11-18 23:13:12 -0300

    Añadiendo paginas variadas, menu y carpeta de datos base para funcionamientio simple.

---
+++ app/Code/Logic/Paciente.py
@@ -0,0 +1,14 @@
+
+
+class Paciente:
+    def __init__(self, nombre, rut, telefono):
+        self.nombre = nombre
+        self.rut = rut
+        self.telefono = telefono
+        self.citas = []
+
+    def agendar_cita(self):
+        pass
+
+    def reagendar_cita(self):
+        pass
--- app/app.py
+++ app/app.py
@@ -1,18 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template, redirect, request, Response, session
+from flask_mysqldb import MySQL, MySQLdb
+from Code.Logic.Especialista import Especialista
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return "<h1>Hola mundo<h1>"
-    integrantes = ["Esteban", "Benjamin", "Martin", "Sebastian", "Joaquin"]
-    data = {
-        "titulo": "index",
-        "bienvenida": "¡Que onda!",
-        "integrantes": integrantes,
-        "num_in": len(integrantes)
-    }
-    return render_template(('index.html'), data=data)
+    return render_template(('menu_incial.html'))
 
+@app.route('/contacto')
+def contacto():
+    with open('Data/datosCentroMedico.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return render_template('informacionCentroMedico.html', data=data['institucion'])
+
+@app.route('/agendar-cita', methods=['POST'])
+def agendar_cita():
+    especialidad = request.form['especialidad']
+    fecha = request.form['fecha']
+
+    especialistas = Especialista.get_especialistas(especialidad)
+    return render_template('agendar_cita.html', especialistas=especialistas, especialidad=especialidad, fecha=fecha)  
+
+@app.route('/confirmar-cita', methods=['GET', 'POST'])
+def confirmar_cita():
+    if request.method == 'POST':
+        try:
+            rut = request.form['rut']
+            especialista = Especialista.get_especialista(rut)
+            fecha = request.form['fecha']
+            hora = request.form['hora']
+
+            return render_template('confirmar_cita.html', especialidad=especialista.especialidad, fecha=fecha, hora=hora, especialista=especialista, nombre=especialista.nombre)
+
+        except KeyError as e:
+            print(f"Error: El campo {e} no se encuentra en el formulario")
+            return "Error: Faltan campos en el formulario", 400
+
+    return render_template('agendar_cita.html')
+    
 if __name__ == '__main__':
     app.run(debug=True, port=1928)
